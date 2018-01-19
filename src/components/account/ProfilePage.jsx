59f34c9a15147a2fe51a298761ecd5558145bfd7
msmedes <smichaelmedes@gmail.com>
2018-01-19 15:19:16 -0500

    react 36

--- src/components/account/ProfilePage.jsx
+++ src/components/account/ProfilePage.jsx
@@ -5,7 +5,7 @@ export default function ProfilePage(props) {
   return (
     <section className="page-content">
       <div className="row">
-        <div className="col-sm-12 col-lg-8">
+        <div className="col-sm-12 col-md-8">
         This is the profile page. The profile id is: {match.params.id}
         </div>
         <aside className="col-sm-12 col-lg-4">
