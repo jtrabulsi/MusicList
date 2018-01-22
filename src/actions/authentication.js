29c918ec3a6bd35e9ad764ebe066c11c75560ab9
msmedes <smichaelmedes@gmail.com>
2018-01-22 17:46:56 -0500

    bricked this dang project at react 46

---
+++ src/actions/authentication.js
@@ -0,0 +1,4 @@
+export const loginAttempt = () => ({ type: 'AUTHENTICATION_LOGIN_ATTEMPT' });
+export const loginFailure = error => ({ type: 'AUTHENTICATION_LOGIN_FAILURE', error });
+export const loginSuccess = json => ({ type: 'AUTHENTICATION_LOGIN_SUCCESS', json });
+
