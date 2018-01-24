e84868fbbf625c9decc9836a6c25fbe9b8782cba
Julia Trabulsi <juliatrabulsi@Julias-MacBook.home>
2018-01-23 22:51:13 -0500

    i think I fixed this shit

--- src/components/account/LoginPageContainer.jsx
+++ src/components/account/LoginPageContainer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import 'whatwg-fetch';
-import { Redirect } from 'react-router-dom';
 
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -14,30 +13,16 @@ export class LoginPageContainer extends React.Component {
 
     // bound functions
     this.attemptLogIn = this.attemptLogIn.bind(this);
-
-    // component state
-    this.state = {
-      redirect: false,
-    };
   }
 
   async attemptLogIn(userData) {
-    const {
-      decrementProgressAction,
-      incrementProgressAction,
-      loginAttemptAction,
-      loginFailureAction,
-      loginSuccessAction,
-    } = this.props;
+    const { decrementProgressAction, incrementProgressAction } = this.props;
 
     // turn on spinner
     incrementProgressAction();
 
-    // register that a login attempt is being made
-    loginAttemptAction();
-
     // contact login API
-    await fetch(
+    const loginResponse = await fetch(
       // where to contact
       '/api/authentication/login',
       // what to send
@@ -49,36 +34,15 @@ export class LoginPageContainer extends React.Component {
         },
         credentials: 'same-origin',
       },
-    )
-    .then((response) => {
-      if (response.state === 200) {
-        return response.json();
-      }
-      return null;
-    })
-    .then((json) => {
-      if (json) {
-        loginSuccessAction(json);
-        this.setState({ redirect: true });
-      } else {
-        loginFailureAction(new Error('Authentication Failed'));
-      }
-    })
-    .catch((error) => {
-      loginFailureAction(new Error(error));
-    });
+    );
+
+    console.log(loginResponse);
+
     // turn off spinner
     decrementProgressAction();
   }
 
   render() {
-    const { redirect } = this.state;
-
-    if (redirect) {
-      return (
-        <Redirect to="/" />
-      );
-    }
     return (
       <div>
         <LoginPage loginFunction={this.attemptLogIn} />
@@ -91,10 +55,6 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators({
     incrementProgressAction: incrementProgress,
     decrementProgressAction: decrementProgress,
-    loginAttemptAction: loginAttempt,
-    loginFailureAction: loginFailure,
-    loginSuccessAction: loginSuccess,
   }, dispatch);
 }
-
 export default connect(null, mapDispatchToProps)(LoginPageContainer);
