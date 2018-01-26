6e8431d393bd363729755b8638e5bdd44ad899f7
Julia Trabulsi <juliatrabulsi@Julias-MacBook.local>
2018-01-26 15:20:35 -0500

    small fix react 71

--- models/album.js
+++ models/album.js
@@ -4,12 +4,12 @@ const Schema = mongoose.Schema;
 
 const Album = new Schema({
   title: String,
-  discogId: Number,
+  discogsId: { type: Number, unique: true },
   artists: [Schema.Types.Mixed],
   thumb: String,
   country: String,
   formats: [Schema.Types.Mixed],
-  genres: [Schema,Types.Mixed],
+  genres: [Schema.Types.Mixed],
   images: [Schema.Types.Mixed],
   labels: [Schema.Types.Mixed],
   notes: String,
